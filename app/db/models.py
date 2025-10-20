3687b512e3119059e797dada7868242007216adb
sevapova <slatipova99@gmail.com>
2025-10-20 10:00:24 +0500

    model

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey
-
+from sqlalchemy import Column, Integer, String, Text, Boolean, ForeignKey, Date
 from app.db.database import Base
 
 
@@ -9,13 +8,17 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(length=128), nullable=False, unique=True, index=True)
     hashed_password = Column(String, nullable=False)
-    # add extra fields: first_name(required), last_name, birth_date, phone, email
 
-    # role: admin, user(default), oxirida
+    first_name = Column(String(length=64), nullable=False)
+    last_name = Column(String(length=64), nullable=True)
+    birth_date = Column(Date, nullable=True)
+    phone = Column(String(length=20), nullable=True)
+    email = Column(String(length=128), nullable=True, unique=True)
 
+    role = Column(String(length=16), default="user")  
     def __repr__(self) -> str:
         return f'User(id={self.id}, username={self.username})'
-    
+
 
 class Task(Base):
     __tablename__ = 'tasks'
@@ -25,8 +28,9 @@ class Task(Base):
     description = Column(Text, default='')
     status = Column(Boolean, default=False, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    # add extra fields: category, priority(1-5)
+
+    category = Column(String(length=64), nullable=True)
+    priority = Column(Integer, default=3)  
 
     def __repr__(self) -> str:
         return f'Task(id={self.id}, name={self.name})'
-    
